06051c5ad5dbb69204ce98a7a9cffbee9c700244
Refat <refatmollah805@gmail.com>
2026-06-23 12:07:04 +0200

    Added trailing comma to RadarComponent slots and fixed init.

---
+++ src/mimo/entity/radar_component.py
@@ -0,0 +1,27 @@
+from .entity import Entity
+
+class RadarComponent():
+    __slots__ = ("_entity",)
+    
+    def __init__(self) -> None:
+        self._entity: Entity | None = None
+    
+    @property
+    def entity(self) -> Entity:
+        if self._entity is None:
+            raise RuntimeError("Component not attached.")
+        
+        return self._entity
+    
+    @property
+    def attached(self) -> bool:
+        return self._entity is not None
+    
+    def on_attach(self, entity: Entity) -> None:
+        self._entity = entity
+
+    def on_detach(self) -> None:
+        self._entity = None
+
+    def on_update(self, time: float) -> None:
+        pass
